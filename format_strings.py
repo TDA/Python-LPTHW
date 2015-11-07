65c9ed1a78293a2f5c30b06eafe3b56c55f4edbb
Sai Pc <schand31@asu.edu>
2015-11-07 00:10:43 -0700

    LPTHW format strings

---
+++ format_strings.py
@@ -0,0 +1,5 @@
+my_name = "saipc"
+my_age = 22
+print "hi i am %s and my age is %d" % (my_name, my_age)
+# what happened to EIBTI??
+print "hi i am %s and my age in 3 years: %s" % (my_name, my_age + 3)
