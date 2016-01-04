623dfa13e9e5e08bb6f811d254482c952cefdbbf
Sai Pc <schand31@asu.edu>
2016-01-04 22:50:01 +0530

    input and raw_input difference

--- input.py
+++ input.py
@@ -17,3 +17,11 @@ age = raw_input("How old are you? ")
 profession = raw_input("What do you do? ")
 
 print "Hi %s, you are %s years old, and you %s" % (name, age, profession)
+
+# Apparently input(prompt) === eval(raw_input(prompt))
+# so these will search for variables named Sai, 22 and Work instead of strings :D
+name = eval(raw_input("What is your name? "))
+age = eval(raw_input("How old are you? "))
+profession = eval(raw_input("What do you do? "))
+
+print "Hi %s, you are %s years old, and you %s" % (name, age, profession)
