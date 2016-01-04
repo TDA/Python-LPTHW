30b3d12cbd7eb3bc58ee14baa5d357d2484dda1b
Sai Pc <schand31@asu.edu>
2016-01-04 22:47:24 +0530

    Inputs and outputs revised

--- input.py
+++ input.py
@@ -1,12 +1,19 @@
 __author__ = 'saipc'
 
-#notice the newlines and no-newlines
-print "What is your name?",
-# always take raw_input instead of input, input takes in strings enclosed in quotes
-name = raw_input()
-print "How old are you?",
-age = raw_input()
-print "What do you do?"
-profession = raw_input()
+# #notice the newlines and no-newlines
+# print "What is your name?",
+# # always take raw_input instead of input, input takes in strings enclosed in quotes
+# name = raw_input()
+# print "How old are you?",
+# age = raw_input()
+# print "What do you do?"
+# profession = raw_input()
+#
+# print "Hi %s, you are %s years old, and you %s" % (name, age, profession)
+
+# Does the same, well, almost, without newlines and spaces
+name = raw_input("What is your name? ")
+age = raw_input("How old are you? ")
+profession = raw_input("What do you do? ")
 
 print "Hi %s, you are %s years old, and you %s" % (name, age, profession)
