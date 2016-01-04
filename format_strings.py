adc48e69b7ac694005a1d9e0e1e310cf8015916f
Sai Pc <schand31@asu.edu>
2016-01-04 22:24:03 +0530

    some more print stuff

--- format_strings.py
+++ format_strings.py
@@ -25,3 +25,16 @@ end12 = "r"
 # watch that comma at the end.  try removing it to see what happens
 print end1 + end2 + end3 + end4 + end5 + end6,
 print end7 + end8 + end9 + end10 + end11 + end12
+
+formatter = "%r\n %r %r %r"
+
+print formatter % (1, 2, 3, 4)
+print formatter % ("one", "two", "three", "four")
+print formatter % (True, False, False, True)
+print formatter % (formatter, formatter, formatter, formatter)
+print formatter % (
+    "I had this thing.",
+    "That you could type up right.",
+    "But it didn't sing.",
+    "So I said goodnight."
+)
