75529ed2e2bb228119ab86084516e682255f2824
Sai Pc <schand31@asu.edu>
2016-01-04 22:57:11 +0530

    Command line args with conventional style

--- command_line_inputs.py
+++ command_line_inputs.py
@@ -1,10 +1,17 @@
 __author__ = 'saipc'
 
 from sys import argv
-
 # unpacks and stores :D
 script_name, first_arg, second_arg = argv
 
 print "Script name is %s" % script_name
 print "First arg is %s" % first_arg
 print "Second arg is %s" % second_arg
+
+# starts with 0 as expected :D
+print "Script name is %s" % argv[0]
+print "First arg is %s" % argv[1]
+print "Second arg is %s" % argv[2]
+
+# Looks like argc isnt here :(
+# print "Total number of args is %s" % argc
