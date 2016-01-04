7dc3d3ef142c4b942ee21d7150139a7fa4f05c8d
Sai Pc <schand31@asu.edu>
2016-01-04 22:20:20 +0530

    More format strings

--- format_strings.py
+++ format_strings.py
@@ -3,3 +3,25 @@ my_age = 22
 print "hi i am %s and my age is %d" % (my_name, my_age)
 # what happened to EIBTI??
 print "hi i am %s and my age in 3 years: %s" % (my_name, my_age + 3)
+
+print "Mary had a little lamb."
+print "Its fleece was white as %s." % 'snow'
+print "And everywhere that Mary went."
+print "." * 10  # what'd that do?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end.  try removing it to see what happens
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
