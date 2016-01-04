2804e41d201dfb9e728b99e1a5a8b88329fd6667
Sai Pc <schand31@asu.edu>
2016-01-04 22:55:19 +0530

    Command line args

---
+++ command_line_inputs.py
@@ -0,0 +1,10 @@
+__author__ = 'saipc'
+
+from sys import argv
+
+# unpacks and stores :D
+script_name, first_arg, second_arg = argv
+
+print "Script name is %s" % script_name
+print "First arg is %s" % first_arg
+print "Second arg is %s" % second_arg
