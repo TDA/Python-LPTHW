f00b0ffc7e7a05d32a0ab11f61d6379b177c4431
Sai Pc <schand31@asu.edu>
2016-01-05 20:00:49 +0530

    Some sorting and word manipulation

--- string_functions_demo.py
+++ string_functions_demo.py
@@ -3,6 +3,20 @@ __author__ = 'saipc'
 from string_functions import *
 
 string = "Hello I am Sai, and I am a programmer (hopefully)"
-words = break_words(string)
 
+words = break_words(string)
 print words
+
+sorted_words = sort_words(words)
+print sorted_words
+
+# does the same as breaking and sorting
+print sort_sentence(string) == sorted_words
+
+print_first_word(sorted_words)
+# did it pop the actual list as well?
+print sorted_words
+# sadly yes, so references, not copies
+
+print_last_word(sorted_words)
+print sorted_words
